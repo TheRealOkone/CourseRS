98d1ba43237e70eeb600335b309e9ebcbf99962c
TheRealOkone <tnoko@yandex.ru>
2022-05-13 17:02:50 +0300

    comm1

--- src/main/java/com/course/coursers/config/SpringJdbcConfig.java
+++ src/main/java/com/course/coursers/config/SpringJdbcConfig.java
@@ -14,9 +14,9 @@ public class SpringJdbcConfig {
     public DataSource mysqlDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/pg_course");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("root");
+        dataSource.setUrl("jdbc:postgres://qzdhjgztbhxcft:b75d541f11a138fb9f037315ec2eaf1c92b099a42349c6c960d9fc742aaf368d@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/d9569fjoc1v0la");
+        dataSource.setUsername("qzdhjgztbhxcft");
+        dataSource.setPassword("b75d541f11a138fb9f037315ec2eaf1c92b099a42349c6c960d9fc742aaf368d");
 
         return dataSource;
     }
